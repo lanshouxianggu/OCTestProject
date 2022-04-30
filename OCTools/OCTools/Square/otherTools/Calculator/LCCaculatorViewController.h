712b3be5c7243a99218d32116ab79c03193d541a
liuchuang <867441694@qq.com>
2022-04-30 17:14:42 +0800

    no message

---
+++ OCTools/OCTools/Square/otherTools/Calculator/LCCaculatorViewController.h
@@ -0,0 +1,16 @@
+//
+//  LCCaculatorViewController.h
+//  OCTools
+//
+//  Created by 刘创 on 2022/4/3.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LCCaculatorViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OCTools/OCTools/Square/otherTools/Calculator/LCCaculatorViewController.m
@@ -0,0 +1,25 @@
+//
+//  LCCaculatorViewController.m
+//  OCTools
+//
+//  Created by 刘创 on 2022/4/3.
+//
+
+#import "LCCaculatorViewController.h"
+
+@interface LCCaculatorViewController ()
+
+@end
+
+@implementation LCCaculatorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.navigationItem.title = @"计算器";
+    self.view.backgroundColor = UIColorFromRGB(0xf2f2f2);
+}
+
+
+
+@end
